4addd028a5a638cbcc340b90b92a0c4a5897606a
kleyson castro <kleysoncastro99@gmail.com>
2020-09-01 15:18:31 -0400

    Styles: styles route/page repository 	sylyes page repository done

--- src/router/Repository/index.tsx
+++ src/router/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +11,48 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <h1>Repository:{params.repository}</h1>
+    <>
+      <Header>
+        <img src={logo} alt="github explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="repos" />
+          <div>
+            <strong>Facebook/react-native</strong>
+            <p>Descriçaõ do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1231</strong>
+            <span>start</span>
+          </li>
+          <li>
+            <strong>11</strong>
+            <span>fork</span>
+          </li>
+          <li>
+            <strong>1231</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>repo.full_name</strong>
+            <p>repo.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
